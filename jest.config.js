f4654d67cf34fbc66ee74552660f6619dd32360b
Nicolas Martins De Oliveira <nicolas.oliveira@ifood.com.br>
2025-06-17 21:48:30 -0300

    feat: add sonarclound coverage

--- jest.config.js
+++ jest.config.js
@@ -10,6 +10,31 @@ const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testEnvironment: 'jest-environment-jsdom',
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/**/*.test.{js,jsx,ts,tsx}',
+    '!src/**/*.spec.{js,jsx,ts,tsx}',
+    '!src/**/__tests__/**',
+    '!src/**/types/**',
+    '!src/**/templates/**',
+    '!src/constants/**',
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: [
+    'text',
+    'text-summary',
+    'lcov',
+    'html'
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 50,
+      functions: 50,
+      lines: 50,
+      statements: 50
+    }
+  }
 }
 
 // createJestConfig é exportado dessa forma para garantir que next/jest possa carregar a configuração do Next.js, que é assíncrona
