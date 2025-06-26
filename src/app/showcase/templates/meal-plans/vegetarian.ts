1062178bb18b677804ca1e4630d6bcf03a34f231
FelipeCorerato <fcorerato@gmail.com>
2025-06-26 16:50:54 -0300

    refactor: [templates] centralize foods and reduce code duplication in meal plans

--- src/app/showcase/templates/meal-plans/vegetarian.ts
+++ src/app/showcase/templates/meal-plans/vegetarian.ts
@@ -1,3 +1,5 @@
+import { shakeProteinaVegana, aveia, leiteAmendoas, banana, iogurteVegano, castanhas, graoBico, arrozIntegral, legumesCozidos, saladaFolhas, azeiteOliva, paoIntegral, hummus, tomate, tofuGrelhado, quinoa, nozes } from './foods';
+
 export const vegetarianMenu = {
   "totalizers": {
     "calorias": 2320,
@@ -8,193 +10,27 @@ export const vegetarianMenu = {
   "refeicoes": [
     {
       "refeicao": "Café da Manhã",
-      "alimentos": [
-        {
-          "alimento": "Shake de Proteína Vegana",
-          "quantidade": "2 scoops",
-          "proteina": "30g",
-          "carboidrato": "15g",
-          "gordura": "10g",
-          "calorias": "270 cal"
-        },
-        {
-          "alimento": "Aveia",
-          "quantidade": "50g",
-          "proteina": "6g",
-          "carboidrato": "34g",
-          "gordura": "3g",
-          "calorias": "180 cal"
-        },
-        {
-          "alimento": "Leite de Amêndoas",
-          "quantidade": "300ml",
-          "proteina": "2g",
-          "carboidrato": "4g",
-          "gordura": "3g",
-          "calorias": "40 cal"
-        },
-        {
-          "alimento": "Banana",
-          "quantidade": "1 unidade",
-          "proteina": "1g",
-          "carboidrato": "27g",
-          "gordura": "0g",
-          "calorias": "105 cal"
-        }
-      ]
+      "alimentos": [shakeProteinaVegana, aveia, leiteAmendoas, banana]
     },
     {
       "refeicao": "Lanche da Manhã",
-      "alimentos": [
-        {
-          "alimento": "Iogurte Vegano",
-          "quantidade": "200g",
-          "proteina": "5g",
-          "carboidrato": "15g",
-          "gordura": "3g",
-          "calorias": "110 cal"
-        },
-        {
-          "alimento": "Castanhas",
-          "quantidade": "30g",
-          "proteina": "4g",
-          "carboidrato": "6g",
-          "gordura": "14g",
-          "calorias": "180 cal"
-        }
-      ]
+      "alimentos": [iogurteVegano, castanhas]
     },
     {
       "refeicao": "Almoço",
-      "alimentos": [
-        {
-          "alimento": "Grão-de-Bico",
-          "quantidade": "150g",
-          "proteina": "9g",
-          "carboidrato": "27g",
-          "gordura": "2g",
-          "calorias": "150 cal"
-        },
-        {
-          "alimento": "Arroz Integral",
-          "quantidade": "100g",
-          "proteina": "4g",
-          "carboidrato": "25g",
-          "gordura": "1g",
-          "calorias": "120 cal"
-        },
-        {
-          "alimento": "Legumes Cozidos",
-          "quantidade": "100g",
-          "proteina": "2g",
-          "carboidrato": "8g",
-          "gordura": "0g",
-          "calorias": "40 cal"
-        },
-        {
-          "alimento": "Salada de Folhas Verdes",
-          "quantidade": "100g",
-          "proteina": "1g",
-          "carboidrato": "2g",
-          "gordura": "0g",
-          "calorias": "15 cal"
-        },
-        {
-          "alimento": "Azeite de Oliva",
-          "quantidade": "1 colher de sopa",
-          "proteina": "0g",
-          "carboidrato": "0g",
-          "gordura": "14g",
-          "calorias": "120 cal"
-        }
-      ]
+      "alimentos": [graoBico, arrozIntegral, legumesCozidos, saladaFolhas, azeiteOliva]
     },
     {
       "refeicao": "Lanche da Tarde",
-      "alimentos": [
-        {
-          "alimento": "Pão Integral",
-          "quantidade": "2 fatias",
-          "proteina": "6g",
-          "carboidrato": "30g",
-          "gordura": "2g",
-          "calorias": "150 cal"
-        },
-        {
-          "alimento": "Hummus",
-          "quantidade": "50g",
-          "proteina": "3g",
-          "carboidrato": "10g",
-          "gordura": "6g",
-          "calorias": "100 cal"
-        },
-        {
-          "alimento": "Tomate",
-          "quantidade": "1 unidade",
-          "proteina": "1g",
-          "carboidrato": "4g",
-          "gordura": "0g",
-          "calorias": "20 cal"
-        }
-      ]
+      "alimentos": [paoIntegral, hummus, tomate]
     },
     {
       "refeicao": "Jantar",
-      "alimentos": [
-        {
-          "alimento": "Tofu Grelhado",
-          "quantidade": "150g",
-          "proteina": "20g",
-          "carboidrato": "3g",
-          "gordura": "10g",
-          "calorias": "190 cal"
-        },
-        {
-          "alimento": "Quinoa",
-          "quantidade": "100g",
-          "proteina": "4g",
-          "carboidrato": "18g",
-          "gordura": "2g",
-          "calorias": "110 cal"
-        },
-        {
-          "alimento": "Legumes Cozidos",
-          "quantidade": "100g",
-          "proteina": "2g",
-          "carboidrato": "8g",
-          "gordura": "0g",
-          "calorias": "40 cal"
-        },
-        {
-          "alimento": "Azeite de Oliva",
-          "quantidade": "1 colher de sopa",
-          "proteina": "0g",
-          "carboidrato": "0g",
-          "gordura": "14g",
-          "calorias": "120 cal"
-        }
-      ]
+      "alimentos": [tofuGrelhado, quinoa, legumesCozidos, azeiteOliva]
     },
     {
       "refeicao": "Ceia",
-      "alimentos": [
-        {
-          "alimento": "Leite de Amêndoas",
-          "quantidade": "300ml",
-          "proteina": "2g",
-          "carboidrato": "4g",
-          "gordura": "3g",
-          "calorias": "40 cal"
-        },
-        {
-          "alimento": "Nozes",
-          "quantidade": "30g",
-          "proteina": "6g",
-          "carboidrato": "4g",
-          "gordura": "20g",
-          "calorias": "220 cal"
-        }
-      ]
+      "alimentos": [leiteAmendoas, nozes]
     }
   ]
 }
